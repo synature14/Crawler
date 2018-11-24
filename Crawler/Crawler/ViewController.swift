bb33c5c6c068b03c487bcd653fbdaf2158332c39
suyeon <synature14@gmail.com>
2018-11-24 18:42:54 +0900

    정규식 이용하여 <a href 하위 주소 추출

--- Crawler/Crawler/ViewController.swift
+++ Crawler/Crawler/ViewController.swift
@@ -11,15 +11,18 @@ import Foundation
 
 class ViewController: UIViewController {
 
-    let clienURL = URL(string: "https://www.clien.net/service/group/allsell")
+    let clienURL = URL(string: "https://www.clien.net")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getData(url: clienURL!)
+        
+        let buyAndSellBoard = clienURL!.appendingPathComponent("/service/board/jirum")
+        print("*** 사고팔고 url = \(buyAndSellBoard)")
+        getData(url: buyAndSellBoard)
     }
     
     func getData(url: URL) {
-        var urlRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 15) // 15초마다 캐시 삭제
+        var urlRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10) // DDOS 공격으로 인식됨을 방지, 15초마다 캐시 삭제
         urlRequest.httpMethod = "GET"
         let dataTask = URLSession.shared.dataTask(with: urlRequest, completionHandler: { data, response, error in
             guard error == nil else {
@@ -37,19 +40,48 @@ class ViewController: UIViewController {
     
     private func updateHTML(_ url: URL) {
         do {
-            let dataString = try String(contentsOf: url)
-            let htmlData = NSString(string: dataString).data(using: String.Encoding.unicode.rawValue)
-            let attributedString = try NSMutableAttributedString(data: htmlData ?? Data.init(),
-                                                             options: [NSAttributedString.DocumentReadingOptionKey.documentType : NSAttributedString.DocumentType.html],
-                                                             documentAttributes: nil)
-            print(attributedString)
 
+            let tag = "(?<=<a href=\"/service/board/jirum/)([^\"]+)(?=\")"
+            
+            let reg = try NSRegularExpression(pattern: tag, options: .caseInsensitive)
+            var urlList: [URL] = [url]
+            
+            while let toVisitUrl = urlList.popLast() {
+                let contents = try String(contentsOf: url)
+                // 필요한 내용 추출
+                
+                let matchedStrings = reg.matches(in: contents, options: [], range: NSRange(location: 0, length: contents.utf16.count))
+                    .compactMap { Range($0.range, in: contents) }
+                    .map { String(contents[$0]) }
+                
+                for urlString in matchedStrings {
+                    // URL 검증
+                    // append
+                    urlList.append(url)
+                }
+                
+            }
+            
+//            let sub = contents.substring(with: NSRange(reg.))
+            
+//                contents.removeSubrange(NSRange(location: 0, length: contents.index))
+            
+//            "/service/board/jirum/12861426?od=T31&amp;po=1&amp;category=&amp;groupCd="
+//            "/service/board/jirum/12861404?od=T31&amp;po=1&amp;category=&amp;groupCd="
+//            "/service/board/jirum/12861371?od=T31&amp;po=1&amp;category=&amp;groupCd="
+            
+            
+            let matchedStrings = reg.matches(in: contents, options: [], range: NSRange(location: 0, length: contents.utf16.count))
+                .compactMap { Range($0.range, in: contents) }
+                .map { String(contents[$0]) }
+                .map { urlList.append($0) }
+            print(urlList)
+            print(urlList.count)
+            
+            
         } catch {
-            // error
+            print(error.localizedDescription)
         }
     }
-    
-
-
 }
 
