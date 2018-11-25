426be827f5f15f0b66631537989bb2b6e805483b
suyeon <synature14@gmail.com>
2018-11-25 12:56:07 +0900

    알뜰구매 게시판의 1 페이지~10페이지까지의 게시물 url 파싱

--- Crawler/Crawler/ViewController.swift
+++ Crawler/Crawler/ViewController.swift
@@ -11,77 +11,62 @@ import Foundation
 
 class ViewController: UIViewController {
 
-    let clienURL = URL(string: "https://www.clien.net")
+    let buyAndSellBoard = URL(string: "https://www.clien.net/service/board/jirum/")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let buyAndSellBoard = clienURL!.appendingPathComponent("/service/board/jirum")
-        print("*** 사고팔고 url = \(buyAndSellBoard)")
-        getData(url: buyAndSellBoard)
+        let pageURL = buyAndSellBoard!.absoluteString + "?&od=T31&po="
+        print("*** pageURL = \(pageURL)")
+        eachPage(of: pageURL)
     }
     
-    func getData(url: URL) {
-        var urlRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10) // DDOS 공격으로 인식됨을 방지, 15초마다 캐시 삭제
-        urlRequest.httpMethod = "GET"
-        let dataTask = URLSession.shared.dataTask(with: urlRequest, completionHandler: { data, response, error in
-            guard error == nil else {
-                print("Error... \(String(describing: error))")
-                return
+    func eachPage(of urlString: String) {
+        for pageIndex in 0...10 {
+            if let url = verifyURL(urlString: urlString + String(pageIndex)) {
+                self.subURLs(url)
             }
-            
-            if let _ = data, let _ = response as? HTTPURLResponse {
-                self.updateHTML(url)
-            }
-        })
-        
-        dataTask.resume()
+        }
     }
     
-    private func updateHTML(_ url: URL) {
+    private func subURLs(_ url: URL) {
         do {
 
             let tag = "(?<=<a href=\"/service/board/jirum/)([^\"]+)(?=\")"
             
             let reg = try NSRegularExpression(pattern: tag, options: .caseInsensitive)
             var urlList: [URL] = [url]
+            var visitedURL = Set<URL>()
             
-            while let toVisitUrl = urlList.popLast() {
+            while urlList.popLast() != nil {
                 let contents = try String(contentsOf: url)
-                // 필요한 내용 추출
                 
-                let matchedStrings = reg.matches(in: contents, options: [], range: NSRange(location: 0, length: contents.utf16.count))
+                // 필요한 내용 추출
+                let matchedStrings = reg.matches(in: contents, options: [], range: NSRange(location: 0, length: contents.utf16.count)) // NSString으로 변환하여 length값 읽는것과 동일한 방법
                     .compactMap { Range($0.range, in: contents) }
                     .map { String(contents[$0]) }
                 
                 for urlString in matchedStrings {
                     // URL 검증
-                    // append
-                    urlList.append(url)
+                    if let url = verifyURL(urlString: buyAndSellBoard!.absoluteString + urlString), !visitedURL.contains(url) {
+                        urlList.append(url)
+                        visitedURL.insert(url)
+                    }
                 }
-                
             }
-            
-//            let sub = contents.substring(with: NSRange(reg.))
-            
-//                contents.removeSubrange(NSRange(location: 0, length: contents.index))
-            
-//            "/service/board/jirum/12861426?od=T31&amp;po=1&amp;category=&amp;groupCd="
-//            "/service/board/jirum/12861404?od=T31&amp;po=1&amp;category=&amp;groupCd="
-//            "/service/board/jirum/12861371?od=T31&amp;po=1&amp;category=&amp;groupCd="
-            
-            
-            let matchedStrings = reg.matches(in: contents, options: [], range: NSRange(location: 0, length: contents.utf16.count))
-                .compactMap { Range($0.range, in: contents) }
-                .map { String(contents[$0]) }
-                .map { urlList.append($0) }
-            print(urlList)
-            print(urlList.count)
-            
-            
+            print(visitedURL)
+            print("총 \(visitedURL.count)개")
+
         } catch {
             print(error.localizedDescription)
         }
+    }
+    
+    private func verifyURL(urlString: String) -> URL? {
+        if let url = URL(string: urlString) {
+            return url
+        }
+        return nil
     }
 }
 
